6faef161b2ed66da01ff1c6db34440152bab6ac3
memukherjee <akash3.11.2000@gmail.com>
2025-02-16 14:10:56 +0530

    fix

--- src/main/java/com/loginUsingGoogleAndGithub/demo/config/SecurityConfig.java
+++ src/main/java/com/loginUsingGoogleAndGithub/demo/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import com.loginUsingGoogleAndGithub.demo.service.AuthService;
@@ -15,10 +17,10 @@ import com.loginUsingGoogleAndGithub.demo.service.AuthService;
 public class SecurityConfig {
     @Autowired
     AuthService authService;
-    @Autowired
-    OidcUserService oidcUserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService = new OidcUserService();
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/public/**").permitAll()
